b91c664086a1d26a33c6436b6a5c76b2edd4759e
Vijaykarthik T K <vijaykarthiktk@gmail.com>
2025-02-15 15:21:19 +0530

    docs: remove sorting and searching algorithms from documentation

--- lib/algokit.dart
+++ lib/algokit.dart
@@ -1,9 +1,4 @@
 /// A versatile Dart algorithms library providing implementations of various algorithms.
-///
-/// This library provides various algorithm implementations including:
-/// - Sorting algorithms (bubble sort, quick sort, merge sort)
-/// - Searching algorithms (binary search, linear search, jump search)
-/// - Data Structures
 library algokit;
 
 export 'src/sorting/sorting.dart';
